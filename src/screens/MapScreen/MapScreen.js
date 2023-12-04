505ac5bb7dbb6fe6dd411bb0b94bd30231cae359
kvjp2209 <nguyenthekien2001@gmail.com>
2023-12-04 09:49:05 +0700

    Update Map Screen for ios, mark point

--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -1,15 +1,84 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Modal,
+  PermissionsAndroid,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import Mapbox from '@rnmapbox/maps';
+import Geolocation from '@react-native-community/geolocation';
 
 Mapbox.setAccessToken(
   'sk.eyJ1Ijoia3ZqcDIwMDEiLCJhIjoiY2xwa3p3bjB4MDF2bTJzbXFsbngwbGwyZCJ9.6Z1iRMXeuWcX4Gp1q-Ibbw',
 );
 
 const MapScreen = () => {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [selectedCoordinate, setSelectedCoordinate] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [annotationCoordinate, setAnnotationCoordinate] = useState(null);
+
+  const handleMapClick = event => {
+    const {geometry} = event;
+    setSelectedCoordinate(geometry.coordinates);
+    setModalVisible(true);
+    setAnnotationCoordinate(geometry.coordinates);
+  };
+
+  useEffect(() => {
+    const requestLocationPermission = () => {
+      Geolocation.requestAuthorization();
+      Geolocation.getCurrentPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          setCurrentLocation([longitude, latitude]);
+        },
+        error => {
+          console.error('Error getting current location:', error);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    };
+
+    requestLocationPermission();
+  }, []);
+
   return (
     <View style={styles.page}>
-      <Mapbox.MapView style={styles.map} />
+      <Mapbox.MapView style={styles.map} onPress={handleMapClick}>
+        <Mapbox.Camera zoomLevel={14} centerCoordinate={currentLocation} />
+        {currentLocation && (
+          <Mapbox.PointAnnotation
+            id="marker"
+            coordinate={annotationCoordinate || currentLocation}>
+            <View style={styles.annotationContainer}>
+              <View style={styles.annotationFill} />
+            </View>
+          </Mapbox.PointAnnotation>
+        )}
+      </Mapbox.MapView>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContent}>
+            <Text>
+              Coordinate:{' '}
+              {selectedCoordinate ? JSON.stringify(selectedCoordinate) : ''}
+            </Text>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setModalVisible(false)}>
+              <Text>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -18,15 +87,42 @@ export default MapScreen;
 
 const styles = StyleSheet.create({
   page: {
-    borderWidth: 5,
-    borderColor: 'red',
     flex: 1,
-  },
-  container: {
-    height: '100%',
-    width: '100%',
   },
   map: {
     flex: 1,
+  },
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    borderRadius: 15,
+  },
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'white',
+    transform: [{scale: 0.6}],
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  closeButton: {
+    marginTop: 10,
+    padding: 10,
+    backgroundColor: 'lightgray',
+    borderRadius: 5,
   },
 });
